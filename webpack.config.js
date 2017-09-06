f7489a355c4a3fa86cd369cfead102d7fd130dd0
nontachaiwebdev <sutininter@gmail.com>
2017-09-06 23:14:29 +0700

    feat(webpack): change entry file in webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ const webpack = require('webpack')
 const path = require('path')
 
 module.exports = {
-  entry: path.join(__dirname, 'src', 'app-client.js'),
+  entry: path.join(__dirname, 'src', 'app.js'),
   output: {
     path: path.join(__dirname, 'src', 'static', 'js'),
     filename: 'bundle.js'
