ef28f82e940b2acd560746c10f3a2008aa4a15e9
nontachaiwebdev <sutininter@gmail.com>
2017-09-06 22:55:46 +0700

    feat(webpack): init config webpack file

---
+++ webpack.config.js
@@ -0,0 +1,24 @@
+const webpack = require('webpack')
+const path = require('path')
+
+module.exports = {
+  entry: path.join(__dirname, 'src', 'app-client.js'),
+  output: {
+    path: path.join(__dirname, 'src', 'static', 'js'),
+    filename: 'bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['react', 'es2015']
+          }
+        }
+      }
+    ]
+  }
+}
