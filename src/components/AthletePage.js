6a902798d00cb97a2407137b0b7ec4a84b1b3931
nontachaiwebdev <sutininter@gmail.com>
2017-09-06 23:46:03 +0700

    feat(component): Add Component

---
+++ src/components/AthletePage.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default class AthletePage extends React.Component {
+  render() {
+    return (
+      <div>AthletePage</div>
+    )
+  }
+}
--- src/routes.js
+++ src/routes.js
@@ -1,13 +1,12 @@
 // src/routes.js
 import React from 'react'
 import { Route, IndexRoute } from 'react-router'
-import Layout from './components/Layout';
 import IndexPage from './components/IndexPage';
 import AthletePage from './components/AthletePage';
 import NotFoundPage from './components/NotFoundPage';
 
 const routes = (
-  <Route path="/" component={Layout}>
+  <Route>
     <IndexRoute component={IndexPage}/>
     <Route path="athlete/:id" component={AthletePage}/>
     <Route path="*" component={NotFoundPage}/>
